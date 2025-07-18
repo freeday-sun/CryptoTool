c8bde7ae81a47cafc62f2543206297125e8aaef5
S Zh <szh@java.com>
2025-07-18 23:53:14 +0200

    Init commit

---
+++ src/main/java/cryptoAlgorithms/Caesar.java
@@ -0,0 +1,4 @@
+package cryptoAlgorithms;
+
+public class Caesar {
+}
---
+++ src/main/java/fileProcessor/FileProcessor.java
@@ -0,0 +1,9 @@
+package fileProcessor;
+
+public class FileProcessor {
+
+    public FileProcessor(){}
+
+    public void read(){}
+    public void write(){}
+}
